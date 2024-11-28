917ce3bcd429db3fc37455e790fbada78df6f0dc
6704062617130 <s6704062617130@email.kmutnb.ac.th>
2024-11-28 17:49:40 +0700

    lab1

---
+++ lab1.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int  main()
+{
+	int num1=10;
+	int num2=50;
+	if(num1>num2)
+	{
+		printf("num1 is grater than num2");
+	}
+	if (num2>num1)
+	{
+		printf("num2 is grater than num1")
+	}
+}
